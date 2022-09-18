979f9a60c4a340a5119013c26a83f96fbe2119fe
Your Name <Andrey3434>
2022-09-18 09:53:40 +0300

    решил 6 и 8 задачи

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 1)
+{
+Console.Write($" число {a} нечетное");
+}
+else 
+{
+Console.Write($" число {a} четное");
+}
+
