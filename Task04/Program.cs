89a3d31976bc8e3d8ea56030e548be2e479003e1
Your Name <Andrey3434>
2022-09-18 09:18:15 +0300

    решила вторую задачу

---
+++ Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+int Max = a;
+
+if(b>a)
+{
+Max = b;  //Console.Write($"{number} больше чем {number1}");
+}
+if(c>Max)
+{
+Max = c;
+}
+Console.Write($" наибольшее число из трех введеных {Max}");
+
+
+
+
+// else
+// {
+// Console.Write($"{number1} больше чем {number}"); 
+// }
