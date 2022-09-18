f26de086bd8da01089df0cbad670520da82e039a
Your Name <Andrey3434>
2022-09-18 08:37:40 +0300

    решил вторую  задачу

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if(number>number1)
+{
+Console.Write($"{number} больше чем {number1}");
+}
+else
+{
+Console.Write($"{number1} больше чем {number}"); 
+}
+
