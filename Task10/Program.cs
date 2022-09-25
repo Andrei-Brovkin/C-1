a7c23b31fa36f576b8315985f7e640b7432e6886
Your Name <Andrey3434>
2022-09-25 22:05:58 +0300

    Решил задачу 10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("ведите число чтобы проверить является ли он выходным ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+    if(number <= 5)
+    Console.Write(" этот день не является выходным");    
+    else if (number==6)
+    Console.Write(" этот день выходной");
+    else if(number==7)
+    Console.Write(" этот день выходной");
+    else if (number>8)
+    Console.Write(" необходимо ввыести число  от 1 до 7 ");
+
+
+
